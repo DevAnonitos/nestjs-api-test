d478d26e00340c93e20a0760cb1490574a7f2ced
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-19 17:39:04 +0700

    update-Add-ConfigModule-Prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '.prisma/client';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+    constructor () {
+        super({
+
+        })
+    }
+}
