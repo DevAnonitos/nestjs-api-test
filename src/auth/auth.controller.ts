f36d4aee108af8b16b89a97b16e6c3e46252bdcf
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-21 20:23:57 +0700

    update-Add-FixError

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,7 @@ import {
     Post,
     Req
 } from '@nestjs/common';
+import { Request } from "express";
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -13,12 +14,14 @@ export class AuthController{
     constructor(private authService: AuthService) {}
 
     @Post('signup')
-    signup() {
+    signup(@Req() req: Request) {
+        console.log(req.body);
         return this.authService.signup();
     }
 
     @Post('signin')
-    signin() {
+    signin(@Req() req: Request) {
+        console.log(req);
         return this.authService.signin();
     }
 }
