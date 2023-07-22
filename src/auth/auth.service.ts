c43cc94fca4fd66a15a7715703caa3cf4354e523
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-23 00:01:44 +0700

    update-Add-hashFunc

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -8,10 +9,17 @@ export class AuthService {
     constructor(private prisma: PrismaService) {}
 
     async signup(dto: AuthDto) {
+        // HashPassword Func
+        const roundSalt = 14;
+        const salt = await bcrypt.genSalt(roundSalt);
+        const hassPassword = await bcrypt.hash(dto.password, salt);
+
+        
         return {
             msg: 'I have signup'
         }
     }
+
     async signin(dto: AuthDto) {
         return {
             msg: 'I have signin'
