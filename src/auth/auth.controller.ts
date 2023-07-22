eeb44c6cd9e4e5da67aafa0a1df2575ae78adf62
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-22 22:24:52 +0700

    update-Add-DtoModule

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,24 +4,30 @@ import {
     HttpCode,
     HttpStatus,
     Post,
-    Req
+    Req,
 } from '@nestjs/common';
 import { Request } from "express";
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController{
     constructor(private authService: AuthService) {}
 
     @Post('signup')
-    signup(@Req() req: Request) {
-        console.log(req.body);
+    signup(@Body() dto: AuthDto) {
+        console.log({
+            dto,
+        });
         return this.authService.signup();
     }
 
+
     @Post('signin')
-    signin(@Req() req: Request) {
-        console.log(req);
+    signin(@Body() dto: AuthDto) {
+        console.log({
+            dto,
+        });
         return this.authService.signin();
     }
 }
