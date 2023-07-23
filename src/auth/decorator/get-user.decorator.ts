bacfd412809643c43c3192d7c72898e5c758d334
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-23 22:27:21 +0700

    update-Add-UserDecorator

---
+++ src/auth/decorator/get-user.decorator.ts
@@ -0,0 +1,21 @@
+import * as request from 'supertest';
+import {
+    createParamDecorator,
+    ExecutionContext
+} from "@nestjs/common";
+
+export const GetUser = createParamDecorator(
+    (
+        data: string | undefined,
+        ctx: ExecutionContext,
+    ) => {
+        const request: Express.Request = ctx
+            .switchToHttp()
+            .getRequest();
+        if(data) {
+            return request['user'][data];
+        }
+
+        return request['user'];
+    },
+);
