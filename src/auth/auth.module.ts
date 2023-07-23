bb44cde27b70d45d90ce6a5c1e340edd0ddd5590
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-23 22:01:31 +0700

    update-Add-Module-handler-Auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaModule } from '../prisma/prisma.module';
+
 
 @Module({
-    imports: [PrismaModule],
+    imports: [JwtModule.register({})],
     controllers: [AuthController],
     providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
+
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
 
@@ -10,8 +12,11 @@ export class AuthService {
 
     constructor(
         private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService,
     ) {}
 
+    // Func signUp->logic handle
     async signup(dto: AuthDto) {
         // HashPassword Func
         const roundSalt = 14;
@@ -19,6 +24,7 @@ export class AuthService {
         const hashPassword = await bcrypt.hash(dto.password, salt);
 
         try {
+            // Verify token
             const user = await this.prisma.user.create({
                 data: {
                     email: dto.email,
@@ -28,16 +34,19 @@ export class AuthService {
 
             return user;
         } catch (error: any) {
-            if(error.code === 'P2002') {
+            // Error if exists
+            if (error.code === 'P2002') {
                 throw new ForbiddenException(
                     'Credentials taken',
                 );
             }
+
             console.log(error.message);
             throw error;
         }
     }
 
+    // Func signIn->logic handle
     async signin(dto: AuthDto) {
         return {
             msg: 'I have signin'
