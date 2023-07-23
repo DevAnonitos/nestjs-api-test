fb46f355ccc11fbf938d84fbe6ac79d811c3d481
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-23 17:35:31 +0700

    update-Add-FuncLogicSignUp

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private prisma: PrismaService) {}
+    constructor(
+        private prisma: PrismaService,
+    ) {}
 
     async signup(dto: AuthDto) {
         // HashPassword Func
         const roundSalt = 14;
         const salt = await bcrypt.genSalt(roundSalt);
-        const hassPassword = await bcrypt.hash(dto.password, salt);
+        const hashPassword = await bcrypt.hash(dto.password, salt);
 
-        
-        return {
-            msg: 'I have signup'
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hashPass: hashPassword,
+                },
+            });
+
+            return user;
+        } catch (error: any) {
+            console.log(error.message);
+            throw error;
         }
     }
 
