db64d1419580befc3015ad28afa83f5f4f1cb144
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-24 01:02:20 +0700

    update-Add-JWTModule

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -9,5 +9,27 @@ import { PrismaService } from "../../prisma/prisma.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    
+    constructor(
+        config: ConfigService,
+        private prisma: PrismaService,
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: config.get('JWT_SECRET'),
+        });
+    }
+
+    async validate(payload: {
+        sub: string,
+        email: string,
+    }) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: payload.sub,
+            },
+        });
+
+        delete user.hashPass;
+        return user;
+    }
 }
