4af12dc29478f4f34f349aa2667501076617a34f
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-25 22:00:54 +0700

    update-Add-ConfigTest-WatchApp

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -14,11 +14,31 @@ import { AuthDto } from "../src/auth/dto";
 import { PrismaService } from "../src/prisma/prisma.service";
 
 describe('App e2e', () => {
+    let app: INestApplication;
+    let prisma: PrismaService;
+
     beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
+
+        app = moduleRef.createNestApplication()
+        app.useGlobalPipes(
+            new ValidationPipe({
+                whitelist: true,
+            })
+        );
+        // Init testing host in port 3333
+        await app.init();
+        await app.listen(3333);
+
     });
 
+    // Close app test
+    afterAll(() => {{
+        app.close();
+    }});
+
     it.todo('should pass');
+    it.todo('should pass 2');
 })
