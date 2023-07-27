477f4143d0432b8f80287c81d139ca9da4a0add0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-27 23:44:03 +0700

    update-Add_SwaggerUI

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //SwaggerModule
+  const options = new DocumentBuilder()
+    .setTitle("ApiTest")
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .addBearerAuth()
+    .build();
+  // Config document
+  const document = SwaggerModule.createDocument(app,options);
+  SwaggerModule.setup('api', app, document);
   // Config PipLine
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,5 +25,6 @@ async function bootstrap() {
   // Add Cors app
   app.enableCors();
   await app.listen(3333);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
