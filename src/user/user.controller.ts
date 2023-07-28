75ec63e6ed83d3ebf498cfcc95258925bb8c53fe
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-28 10:11:10 +0700

    update-AddUser-Controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,15 +7,27 @@ import {
 } from "@nestjs/common";
 import { User } from "@prisma/client";
 import { UserService } from "./user.service";
+import { GetUser } from "../auth/decorator";
+import { EditUserDto } from "./dto";
+import { JwtGuard } from "../auth/guard";
 
-@Controller('user')
+@UseGuards(JwtGuard)
+@Controller('users')
 export class UserController {
     constructor(private userService: UserService) {}
+    @Get('user')
+    getUsers(@GetUser() user: User) {
+        return user;
+    }
 
     @Patch()
-    editUser() {
-        return {
-            message:'success',
-        }
+    editUser(
+        @GetUser('id') userId: string,
+        @Body() dto: EditUserDto,
+    ) {
+        console.log({
+            message: dto,
+        })
+        return this.userService.editUser(userId, dto);
     }
 }
