f3e9795486f1e6e28b2d32495691c7b692cdd3e1
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-30 22:51:15 +0700

    update-Add-import->BookMarkModule

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,12 @@
+import {
+    ForbiddenException,
+    Injectable
+} from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+
+@Injectable()
+export class BookmarkService {
+    constructor(private prisma: PrismaService) {}
+
+    
+}
