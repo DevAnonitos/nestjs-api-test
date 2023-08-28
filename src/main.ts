097f153c89f8ed412e39ecf921db307a4961bf27
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-28 22:42:22 +0700

    update:Add->MicroService

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //SwaggerModule
